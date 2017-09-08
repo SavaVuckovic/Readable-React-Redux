a86a40e06633a4074d4caab89a15c5b3e1e358d8
Sava <vuckovicsava11@gmail.com>
2017-09-08 20:10:17 +0200

    feat: random comment image feature implemented

---
+++ client/src/components/comment.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  chooseImageRandomly() {
+    const randNum = Math.floor(Math.random()*7+1);
+    return require(`../images/user_${ randNum }.jpg`);
+  }
+
+  render() {
+    return (
+      <div className="comment">
+        <div className="comment-header">
+          <div className="comment-img">
+            <img src={this.chooseImageRandomly()} alt="user" />
+          </div>
+          <div className="comment-author">
+            <h4>Sava Vuckovic</h4>
+            <span>Some timestamp</span>
+          </div>
+        </div>
+        <div className="comment-text">
+          jashjlsa hdkjashdk sajhdjks hak asdsdad aas
+          jashjlsa hdkj jashjl sa hdkja shdk sajhdjks hak a
+          sdsdad aasashd
+          k sajhdjks hak asdsdad aas
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Comment;
--- client/src/components/comments.js
+++ client/src/components/comments.js
@@ -1,51 +1,19 @@
 import React, { Component } from 'react';
+import Comment from './comment';
 import '../styles/comments.css';
 
 class Comments extends Component {
-  renderComment() {
-    return (
-      <div className="comment">
-
-        <div className="comment-header">
-
-          <div className="comment-img">
-            <img src={require('../images/user_3.jpg')} alt="user" />
-          </div>
-
-          <div className="comment-author">
-            <h4>Sava Vuckovic</h4>
-            <span>Some timestamp</span>
-          </div>
-
-        </div>
-
-        <div className="comment-text">
-          jashjlsa hdkjashdk sajhdjks hak asdsdad aas
-        </div>
-
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="comments">
-        { this.renderComment() }
-        { this.renderComment() }
-        { this.renderComment() }
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     );
   }
 }
 
 export default Comments;
-
-/*
-
-<img src={require('../images/user_3.jpg')} alt="user" />
-<div className="user">
-  <h4>Sava Vuckovic</h4>
-  <span>Some timestamp</span>
-</div>
-
-*/
