c0b2efc64587157b1c3e6f1df10788e6daded5bb
Sava <vuckovicsava11@gmail.com>
2017-09-13 12:56:55 +0200

    feat: add-post layout finished

--- client/src/components/addpost.js
+++ client/src/components/addpost.js
@@ -10,26 +10,31 @@ class AddPost extends Component {
   render() {
     return (
       <div className="row justify-content-md-center">
-        <form
-          className="add-post-form offset-md-3 col-md-6"
-          onSubmit={this.onFormSubmit}>
-          <input
-            type="text"
-            className="form-field"
-            placeholder="Title" />
-          <input
-            type="text"
-            className="form-field"
-            placeholder="Author" />
-          <select className="category-select form-field">
-            <option selected>Select category</option>
-            <option>Category 1</option>
-            <option>Category 2</option>
-            <option>Category 3</option>
-          </select>
-          <textarea className="post-body-field form-field"></textarea>
-          <button type="submit">Post</button>
-        </form>
+        <div className="add-post offset-md-3 col-md-6">
+          <div className="add-post-header">
+            <h2>Add Post</h2>
+          </div>
+          <form
+            className="add-post-form"
+            onSubmit={this.onFormSubmit}>
+            <input
+              type="text"
+              className="form-field"
+              placeholder="Title" />
+            <input
+              type="text"
+              className="form-field"
+              placeholder="Author" />
+            <select className="category-select form-field">
+              <option selected>Select category</option>
+              <option>Category 1</option>
+              <option>Category 2</option>
+              <option>Category 3</option>
+            </select>
+            <textarea className="post-body-field form-field"></textarea>
+            <button type="submit">Post</button>
+          </form>
+        </div>
       </div>
     );
   }
