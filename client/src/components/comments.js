f2536cc9595bf8344d35c4c36c39ee1dfaa9047e
Sava <vuckovicsava11@gmail.com>
2017-09-28 20:26:02 +0200

    refactor: SinglePost refactored

--- client/src/components/comments.js
+++ client/src/components/comments.js
@@ -1,41 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Comment from './comment';
 import '../styles/comments.css';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { getComments } from '../actions';
 
-class Comments extends Component {
-  // fetch comments for particular post from the api server
-  componentWillMount() {
-    this.props.getComments(this.props.postID);
-  }
-
-  // render <Comment /> component for each comment
-  render() {
-    return (
-      <div className="comments row">
-        {this.props.comments.map((comment) => {
-          return (
-            <Comment key={comment.id} comment={comment} />
-          );
-        })}
-      </div>
-    );
-  }
+// render <Comment /> component for each comment
+const Comments = (props) => {
+  return (
+    <div className="comments">
+      {props.comments.map((comment) => {
+        return (
+          <Comment key={comment.id} comment={comment} />
+        );
+      })}
+    </div>
+  );
 }
 
-// map comments that belong to this post to props
-function mapStateToProps({ comments }, ownProps) {
-  const thisPostComments = comments.filter((comm) => {
-    return comm.parentId === ownProps.postID;
-  });
-  return { comments: thisPostComments };
-}
-
-// map action for fetching comments to props
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ getComments }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Comments);
+export default Comments;
--- client/src/routes/singlepost.js
+++ client/src/routes/singlepost.js
@@ -1,15 +1,161 @@
-import React from 'react';
-import FullPost from '../components/fullpost';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getSinglePost, deletePost, getComments } from '../actions';
+import { Link } from 'react-router-dom';
+import Modal from '../components/modal';
+import AddCommentForm from '../components/addcommentform';
 import Comments from '../components/comments';
 
 // render single post page
-const SinglePost = (props) => {
+class SinglePost extends Component {
+  // fetch the post and its comments from the api server
+  componentWillMount() {
+    this.props.getSinglePost(this.props.match.params.post_id);
+    this.props.getComments(this.props.match.params.post_id);
+  }
+
+  // show comment modal
+  showCommentModal() {
+    let modal = this.refs.commentModal.modalTarget;
+    if(modal.style.display !== 'block') {
+      modal.style.display = 'block';
+    }
+  }
+
+  // hide comment modal
+  hideCommentModal() {
+    let modal = this.refs.commentModal.modalTarget;
+    if(modal.style.display !== 'none') {
+      modal.style.display = 'none';
+    }
+  }
+
+  // show delete modal
+  showDeleteModal() {
+    let modal = this.refs.deleteModal.modalTarget;
+    if(modal.style.display !== 'block') {
+      modal.style.display = 'block';
+    }
+  }
+
+  // hide delete modal
+  hideDeleteModal() {
+    let modal = this.refs.deleteModal.modalTarget;
+    if(modal.style.display !== 'none') {
+      modal.style.display = 'none';
+    }
+  }
+
+  // delete post
+  deletePost(id, deletePost) {
+    deletePost(id);
+    this.props.history.push('/');
+  }
+
+  // render a post with all related information and comment section
+  render() {
+    const post = this.props.post;
+    if(!post) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+
     return (
-      <div>
-        <FullPost postID={props.match.params.post_id} />
-        <Comments postID={props.match.params.post_id} />
+      <div className="row">
+
+        <Modal
+          ref="commentModal"
+          header="Add a Comment">
+          <AddCommentForm
+            postID={post.id}
+            hideModal={this.hideCommentModal.bind(this)} />
+        </Modal>
+
+        <Modal
+          ref="deleteModal"
+          header="Delete Post">
+          <h4 className="warning">Are you sure you want to delete this post?</h4>
+          <button
+            className="cancel-btn"
+            onClick={this.hideDeleteModal.bind(this)} >Cancel</button>
+          <button
+            className="delete-btn"
+            onClick={() => this.deletePost(post.id, this.props.deletePost)}>Delete</button>
+          <div className="clearfix"></div>
+        </Modal>
+
+        <div className="fullpost">
+
+          <div className="post-header">
+            <div className="post-header-controlls">
+
+              <Link to="/"><i className="fa fa-arrow-circle-left" aria-hidden="true"></i></Link>
+              <Link to={{
+                pathname: '/posts/create_edit',
+                hash: `#${post.id}`,
+                state: { fromSinglePost: true }
+              }}><i className="fa fa-pencil" aria-hidden="true"></i></Link>
+              <i className="fa fa-trash" aria-hidden="true" onClick={this.showDeleteModal.bind(this)}></i>
+
+            </div>
+            <h2>{post.title}</h2>
+            <p>Posted by {post.author} on {moment(post.timestamp).format('ddd MM. DD. YYYY.')}</p>
+          </div>
+
+          <div className="post-body">
+            {post.body}
+          </div>
+
+          <div className="single-post-footer">
+
+            <div className="post-footer-info">
+              <div>votes: {post.voteScore}</div>
+              <div>comments: 566567</div>
+            </div>
+
+            <div className="post-footer-controlls">
+              <div className="control">
+                <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+              </div>
+              <div className="control">
+                <i className="fa fa-thumbs-down" aria-hidden="true"></i>
+              </div>
+              <div className="control">
+                <i className="fa fa-comments" aria-hidden="true"
+                  onClick={this.showCommentModal.bind(this)}></i>
+              </div>
+            </div>
+
+            <div className="clearfix"></div>
+          </div>
+
+        </div>
+
+        <Comments comments={this.props.comments} />
+
       </div>
     );
+  }
 }
 
-export default SinglePost;
+// map post to show and its comments to props
+function mapStateToProps(state, ownProps) {
+  const thisPostComments = state.comments.filter((comm) => {
+    return comm.parentId === ownProps.match.params.post_id;
+  });
+
+  return {
+    post: state.activePost,
+    comments: thisPostComments
+  };
+}
+
+// map action for fetching and deleting a post, and getting comments to props
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getSinglePost, deletePost, getComments }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SinglePost);
